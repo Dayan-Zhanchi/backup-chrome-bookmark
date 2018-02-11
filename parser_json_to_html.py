be80c10176fc5072431897431f13475eed23fb2d
Harry Vuong <harry_95_v@hotmail.com>
2018-02-11 04:53:21 +0100

    initial working parser

--- parser_json_to_html.py
+++ parser_json_to_html.py
@@ -11,6 +11,17 @@ class Parser_json_to_html:
     # for more information about Netscape bookmark file format visit the link below
     # link: https://msdn.microsoft.com/en-us/library/aa753582(v=vs.85).aspx
 
+    def __init__(self):
+        # get json file and convert to readable chrome bookmark html file
+        self.output_html_file = """<!DOCTYPE NETSCAPE-Bookmark-file-1>
+        <!-- This is an automatically generated file.
+        It will be read and overwritten.
+        DO NOT EDIT! -->
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+        <TITLE>Bookmarks</TITLE>
+        <H1>Bookmarks</H1>
+        <DL><p>\n""" 
+
     def __write_new_file(self, dest_path_local, read_data):
         with open(dest_path_local, 'w', encoding="utf8") as new_f:
                 new_f.write(read_data)
@@ -69,31 +80,89 @@ class Parser_json_to_html:
             print("It is time to timeout")
             print(str(e))
 
-
     def __convert_chrometimestamp_to_unixtimestamp(self, chrome_timestamp):
         # chrome timestamp is in microseconds since Jan 1, 1601 and is 1/10 of windows timestamp
         # unix timestamp is in seconds since Jan 1, 1970
         # the base date-times difference between chrome and unix time format is 11644473600000000 in microseconds
         
-        unix_timestamp = (chrome_timestamp - 11644473600000000) / 10^6 
+        # note it's important to convert the timestamp argument into an int
+        # also important to round the answer down with int
+        chrome_timestamp = int(chrome_timestamp)
+        if (chrome_timestamp - 11644473600000000) <= 0:
+            unix_timestamp = 0
+        else:
+            unix_timestamp = int((chrome_timestamp - 11644473600000000) / 10**6)
         return unix_timestamp
 
-    def __parse_json_to_html_helper(self, json_data, output_html_file, dest_file_path):
-        self.__write_new_file(dest_file_path, json.dumps(json_data['roots']['bookmark_bar'], indent = 4))
-        print(len(json_data['roots']['bookmark_bar']))
+    def __close_DL_p_tag(self):
+        self.output_html_file = "".join((self.output_html_file, "</DL><p>\n"))
 
-    def parse_json_to_html(self, file_path, dest_file_path):
-        # get json file and convert to readable chrome bookmark html file
-        output_html_file = """<!DOCTYPE NETSCAPE-Bookmark-file-1>
-        <!-- This is an automatically generated file.
-        It will be read and overwritten.
-        DO NOT EDIT! -->
-        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-        <TITLE>Bookmarks</TITLE>
-        <H1>Bookmarks</H1>
-        <DL><p>
-        
-        """ 
-        data = self.__get_json_data(file_path)
-        self.__parse_json_to_html_helper(data, output_html_file, dest_file_path)
+    def __create_folder_node(self, name, date, date_modified, output_html_file):
+        self.output_html_file = "".join((output_html_file, '<DT><H3 ADD_DATE="{date}" LAST_MODIFIED="{last_modified}">{name}</H3>\n<DL><p>\n'.format(
+                    name = name, 
+                    date = self.__convert_chrometimestamp_to_unixtimestamp(date),
+                    last_modified = self.__convert_chrometimestamp_to_unixtimestamp(date_modified)
+                    )))
 
+    def __create_url_node(self, url, name, date, output_html_file):
+        self.output_html_file = "".join((output_html_file, '<DT><A HREF="{url}" ADD_DATE="{date}">{name}</A>\n'.format(
+                    url = url,
+                    name = name,
+                    date = self.__convert_chrometimestamp_to_unixtimestamp(date)
+                    )))
+
+    def __parse_json_to_html_helper(self, json_data):
+        #self.__write_new_file(dest_file_path_txt, json.dumps(json_data['roots']['bookmark_bar'], indent = 4))
+        #self.__write_new_file(dest_file_path_html, json.dumps(json_data['roots']['bookmark_bar'], indent = 4))
+        for node in json_data:
+            if 'url' in node:
+                self.__create_url_node(
+                    node['url'],
+                    node['name'], 
+                    node['date_added'],  
+                    self.output_html_file)
+
+            elif node['children'] == []:
+                self.__create_folder_node(
+                    node['name'], 
+                    node['date_added'],
+                    node['date_modified'],
+                    self.output_html_file)
+                self.__close_DL_p_tag()
+
+            elif node['children']:
+                self.__create_folder_node(
+                    node['name'], 
+                    node['date_added'],
+                    node['date_modified'],
+                    self.output_html_file)
+                self.__parse_json_to_html_helper(node['children'])
+                self.__close_DL_p_tag()
+                
+    def parse_json_to_html(self, file_path, dest_file_path_txt, dest_file_path_html):
+        node = self.__get_json_data(file_path)
+
+        if node['roots']['bookmark_bar']:
+            self.output_html_file = "".join((self.output_html_file, '<DT><H3 ADD_DATE="{date}" LAST_MODIFIED="{last_modified}" PERSONAL_TOOLBAR="{boolean}">{name}</H3>\n<DL><p>\n'.format(
+                name = node['roots']['bookmark_bar']['name'], 
+                date = self.__convert_chrometimestamp_to_unixtimestamp(node['roots']['bookmark_bar']['date_added']),
+                last_modified = self.__convert_chrometimestamp_to_unixtimestamp(node['roots']['bookmark_bar']['date_modified']),
+                boolean = "true"
+                )))
+            self.__parse_json_to_html_helper(node['roots']['bookmark_bar']['children'])
+            self.__close_DL_p_tag()
+
+        if node['roots']['other']:
+            self.__create_folder_node(
+                node['roots']['other']['name'], 
+                node['roots']['other']['date_added'],
+                node['roots']['other']['date_modified'],
+                self.output_html_file)
+            self.__parse_json_to_html_helper(node['roots']['other']['children'])
+            self.__close_DL_p_tag()
+
+        if node['roots']['synced']:
+            self.__parse_json_to_html_helper(node['roots']['synced']['children'])
+            self.__close_DL_p_tag()
+
+        return self.output_html_file
